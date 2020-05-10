704c9cad7ede153d82758f7ca1271c0e7f00baa0
sudhir <yadavsudhir405@gmail.com>
2020-05-10 22:51:40 +0530

    Implemented navigate to products page on login

--- APM-Start/src/app/user/login.component.ts
+++ APM-Start/src/app/user/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import {Router} from '@angular/router';
+
 
 import { AuthService } from './auth.service';
 
@@ -10,15 +12,14 @@ export class LoginComponent {
   errorMessage: string;
   pageTitle = 'Log In';
 
-  constructor(private authService: AuthService) { }
+  constructor(private readonly authService: AuthService, private readonly router: Router) { }
 
   login(loginForm: NgForm) {
     if (loginForm && loginForm.valid) {
       const userName = loginForm.form.value.userName;
       const password = loginForm.form.value.password;
       this.authService.login(userName, password);
-
-      // Navigate to the Product List page after log in.
+      this.router.navigate(['/products']);
     } else {
       this.errorMessage = 'Please enter a user name and password.';
     }
