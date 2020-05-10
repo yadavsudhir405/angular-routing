cff150acc9d3664591a9bc8078df35d63043e35f
sudhir <yadavsudhir405@gmail.com>
2020-05-10 23:12:51 +0530

    Seperated routes configuration to seperate module

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,34 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 // Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { ProductData } from './products/product-data';
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {ProductData} from './products/product-data';
 
-import { AppComponent } from './app.component';
-import { WelcomeComponent } from './home/welcome.component';
-import { PageNotFoundComponent } from './page-not-found.component';
-
+import {AppComponent} from './app.component';
+import {WelcomeComponent} from './home/welcome.component';
+import {PageNotFoundComponent} from './page-not-found.component';
 /* Feature Modules */
-import { ProductModule } from './products/product.module';
-import { UserModule } from './user/user.module';
-import { MessageModule } from './messages/message.module';
-import {RouterModule} from '@angular/router';
+import {ProductModule} from './products/product.module';
+import {UserModule} from './user/user.module';
+import {MessageModule} from './messages/message.module';
+import {AppRoutingModule} from './app.routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {path: 'welcome', component: WelcomeComponent},
-      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-      {path: '**', component: PageNotFoundComponent}
-    ]),
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
     ProductModule,
     UserModule,
-    MessageModule
+    MessageModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
