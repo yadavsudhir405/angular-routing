75dace5f8e55876249a4205b3d55eda7afcec189
sudhir <yadavsudhir405@gmail.com>
2020-05-11 22:11:38 +0530

    Implemented child routes for product edit info and tags

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -7,6 +7,8 @@ import {ProductEditComponent} from './product-edit/product-edit.component';
 import {SharedModule} from '../shared/shared.module';
 import {RouterModule} from '@angular/router';
 import {ProductResolver} from './product-resolver.service';
+import {ProductEditInfoComponent} from './product-edit/product-edit-info.component';
+import {ProductEditTagsComponent} from './product-edit/product-edit-tags.component';
 
 const ROUTES = [
   {path: 'products', component: ProductListComponent},
@@ -18,7 +20,12 @@ const ROUTES = [
   {
     path: 'products/:id/edit',
     component: ProductEditComponent,
-    resolve: {resolvedProduct: ProductResolver}
+    resolve: {resolvedProduct: ProductResolver},
+    children: [
+      {path: '', redirectTo: 'info', pathMatch: 'full'},
+      {path: 'info', component: ProductEditInfoComponent},
+      {path: 'tags', component: ProductEditTagsComponent}
+    ]
   }
 ];
 
@@ -30,7 +37,9 @@ const ROUTES = [
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule {
