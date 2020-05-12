f948378d7afc6f6e3035305d2ec88a57e904dcc4
sudhir <yadavsudhir405@gmail.com>
2020-05-12 21:40:55 +0530

    Added spinner while loading the page

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import { AuthService } from './user/auth.service';
-import {Router} from '@angular/router';
+import {Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError
+} from '@angular/router';
 import {slideInAnimation} from './app.animation';
 
 @Component({
@@ -12,7 +13,25 @@ import {slideInAnimation} from './app.animation';
 })
 export class AppComponent {
   pageTitle = 'Acme Product Management';
+  isLoading = false;
 
+  constructor(private authService: AuthService, private readonly router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      this.handleRouterEvent(event);
+    });
+  }
+
+  handleRouterEvent(event: Event) {
+    if (event instanceof NavigationStart) {
+      this.isLoading = true;
+    }
+
+    if (event instanceof NavigationEnd ||
+    event instanceof  NavigationError ||
+    event instanceof  NavigationCancel) {
+      this.isLoading = false;
+    }
+  }
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
@@ -24,8 +43,6 @@ export class AppComponent {
     }
     return '';
   }
-
-  constructor(private authService: AuthService, private readonly router: Router) { }
 
   logOut(): void {
     this.authService.logout();
