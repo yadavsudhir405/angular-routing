f016be8a852df2760139607d9ae827f2b6dff1b0
Sudhir Yadav <sudhir.yadav@kroger.com>
2020-05-12 14:29:00 +0530

    Add slide animation to route changes

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -13,10 +13,12 @@ import {ProductModule} from './products/product.module';
 import {UserModule} from './user/user.module';
 import {MessageModule} from './messages/message.module';
 import {AppRoutingModule} from './app.routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
     ProductModule,
