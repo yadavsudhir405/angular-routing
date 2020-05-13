fb468d08576a7f34079ee80b9d43e467f74fe6d5
sudhir <yadavsudhir405@gmail.com>
2020-05-13 23:03:51 +0530

    Load product module only after login

--- APM-Start/src/app/user/auth-guard.service.ts
+++ APM-Start/src/app/user/auth-guard.service.ts
@@ -1,19 +1,27 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment} from '@angular/router';
 import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private readonly authService: AuthService,
               private readonly router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    return this.checkLogin(state.url);
+  }
+
+  canLoad(route: Route): boolean {
+    return this.checkLogin(route.path);
+  }
+
+  checkLogin(url: string): boolean {
     if (this.authService.isLoggedIn) {
       return true;
     } else {
-      this.authService.redirectUrl = state.url;
+      this.authService.redirectUrl = url;
       this.router.navigate(['/login']);
       return false;
     }
